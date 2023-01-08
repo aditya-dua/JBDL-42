75ef43fdecdc3255ef9e5eb002729f4a3dcd9ee9
Aditya Dua <adityaduatechm@gmail.com>
2023-01-08 21:34:47 +0530

    Update 1

--- hibernate.sample/src/main/java/HibernateMain.java
+++ hibernate.sample/src/main/java/HibernateMain.java
@@ -1,5 +1,6 @@
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateMain {
@@ -7,12 +8,17 @@ public class HibernateMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Student s = new Student(1, "ABC", 12, "12345678");
 		Configuration c = new Configuration();
 		
 		SessionFactory factory = c.configure("hbm.cfg.xml").buildSessionFactory();
 		
 		Session session = factory.openSession();
 		
+		Transaction tx = session.beginTransaction();
+		session.save(s);
+		
+		tx.commit();
 		System.out.println("Session Started");
 		
 		session.close();
---
+++ hibernate.sample/src/main/java/Student.java
@@ -0,0 +1,78 @@
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "students-jbdl-42")
+public class Student {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "age")
+	private int age;
+	
+	@Column(name = "phone")
+	private String phone;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", age=" + age + ", phone=" + phone + "]";
+	}
+
+	public Student(int id, String name, int age, String phone) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.phone = phone;
+	}
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	
+}
