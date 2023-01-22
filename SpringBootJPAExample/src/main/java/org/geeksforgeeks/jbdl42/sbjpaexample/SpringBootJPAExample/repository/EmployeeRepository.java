a58e2e1e077294e7092afc3fdfc7982ce86c7f36
Aditya Dua <adityaduatechm@gmail.com>
2023-01-22 21:19:28 +0530

    Fix -1
    Fix -1

--- SpringBootJPAExample/src/main/java/org/geeksforgeeks/jbdl42/sbjpaexample/SpringBootJPAExample/repository/EmployeeRepository.java
+++ SpringBootJPAExample/src/main/java/org/geeksforgeeks/jbdl42/sbjpaexample/SpringBootJPAExample/repository/EmployeeRepository.java
@@ -1,26 +1,10 @@
 package org.geeksforgeeks.jbdl42.sbjpaexample.SpringBootJPAExample.repository;
 
 import org.geeksforgeeks.jbdl42.sbjpaexample.SpringBootJPAExample.entity.Employee;
-import org.springframework.data.jpa.repository.support.JpaEntityInformation;
-import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import jakarta.persistence.EntityManager;
-
 @Repository
-public class EmployeeRepository extends SimpleJpaRepository<Employee, Integer>{
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
 
-	public EmployeeRepository(Class<Employee> domainClass, EntityManager em) {
-		super(domainClass, em);
-		// TODO Auto-generated constructor stub
-	}
-
-	public EmployeeRepository(JpaEntityInformation<Employee, ?> entityInformation, EntityManager entityManager) {
-		super(entityInformation, entityManager);
-		// TODO Auto-generated constructor stub
-	}
-	
-	
-
-	
 }
